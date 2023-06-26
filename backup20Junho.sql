b7ce567bc8cbd4aeb497a5ac3bd5d2eeaf332282
LenilsonLimaPantoja2 <lenilsonlima648@gmail.com>
2023-06-26 10:35:19 -0400

    26-06-2023 .env and backup20Junho

--- backup20Junho.sql
+++ backup20Junho.sql
@@ -18,38 +18,7 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE locadora;
---
--- TOC entry 3410 (class 1262 OID 24658)
--- Name: locadora; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE locadora WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Portuguese_Brazil.1252';
-
-
-ALTER DATABASE locadora OWNER TO postgres;
-
-\connect locadora
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- TOC entry 214 (class 1259 OID 24659)
--- Name: categoria; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.categoria (
     id integer NOT NULL,
